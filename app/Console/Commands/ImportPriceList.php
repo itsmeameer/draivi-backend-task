4c0508017b51da48e2d534d405987f291cf87f19
Ameer Humza <itsmeameer@gmail.com>
2023-12-05 22:31:08 +0200

    chore: removed backup file

--- app/Console/Commands/ImportPriceList.php
+++ app/Console/Commands/ImportPriceList.php
@@ -70,8 +70,6 @@ class ImportPriceList extends Command
 
         $this->info('File downloaded successfully, validating...');
 
-        $price_list_path = storage_path('files/backup.xlsx');
-
         if (! $this->isExcelFile($price_list_path)) {
             $this->error('The file is not an Excel file.');
             return;
